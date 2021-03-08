d78813dfbcae7f2adb47083c48956bb71ea33ede
jshields123 <jackshields@live.co.uk>
2021-03-08 17:25:28 +0000

    test for empty string and passed

---
+++ lib/scrabble.rb
@@ -0,0 +1,13 @@
+class Scrabble
+
+  def initialize(string)
+    @string = string
+  end
+
+  def score
+    0
+
+  end
+
+
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,40 @@
+require 'scrabble'
+
+describe Scrabble do
+  it 'returns 0 when given an empty string' do
+    scrabble = Scrabble.new('')
+    expect(scrabble.score).to eq 0
+  end
+
+
+end
+
+
+
+
+
+
+# Acceptance Criteria
+# scrabble = Scrabble.new('')
+# scrabble.score # => 0
+#
+# scrabble = Scrabble.new(" \t\n")
+# scrabble.score # => 0
+#
+# scrabble = Scrabble.new(nil)
+# scrabble.score # => 0
+#
+# scrabble = Scrabble.new('a')
+# scrabble.score # => 1
+#
+# scrabble = Scrabble.new('f')
+# scrabble.score # => 4
+#
+# scrabble = Scrabble.new('street')
+# scrabble.score # => 6
+#
+# scrabble = Scrabble.new('quirky')
+# scrabble.score # => 22
+#
+# scrabble = Scrabble.new('OXYPHENBUTAZONE')
+# scrabble.score # => 41
